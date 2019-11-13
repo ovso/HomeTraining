386293f297920d5c7086aa0ec7eee47c823a2aef
jaeho <ovsoce@gmail.com>
2019-11-14 00:56:07 +0900

    ViewModel creation using ViewModelProviders.

--- app/src/main/kotlin/io/github/ovso/hometraining/view/video/VideoItemViewModel.kt
+++ app/src/main/kotlin/io/github/ovso/hometraining/view/video/VideoItemViewModel.kt
@@ -1,15 +1,16 @@
 package io.github.ovso.hometraining.view.video
 
 import android.app.Application
+import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 
-class VideoItemViewModel(app: Application) : AndroidViewModel(app) {
+class VideoItemViewModel(var app: Application) : AndroidViewModel(app) {
 
     var videoId: String? = null
     var imgUrl: String? = null
 
     fun onClick() {
-
+        Toast.makeText(app, "onClick", Toast.LENGTH_SHORT).show()
     }
 
 }
