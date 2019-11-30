9dd105d893f98f6d6e8899280b0ff3d88481a849
jaeho <ovsoce@gmail.com>
2019-11-30 18:56:53 +0900

    Update male recyclerview

--- app/src/main/kotlin/io/github/ovso/hometraining/view/binding/RvBindingAdapter.kt
+++ app/src/main/kotlin/io/github/ovso/hometraining/view/binding/RvBindingAdapter.kt
@@ -2,6 +2,7 @@ package io.github.ovso.hometraining.view.binding
 
 import androidx.databinding.BindingAdapter
 import androidx.databinding.ObservableList
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 import io.github.ovso.hometraining.view.male.MaleRvAdapter
 
@@ -13,6 +14,7 @@ object RvBindingAdapter {
     rv: RecyclerView,
     items: ObservableList<String>
   ) {
+    rv.addItemDecoration(DividerItemDecoration(rv.context, DividerItemDecoration.VERTICAL))
     rv.adapter = MaleRvAdapter().apply { titles.addAll(items) }
   }
 }
