5be962abcc0b612f29bae4f49b3f7991d9a84515
jaeho <ovsoce@gmail.com>
2019-11-30 19:51:50 +0900

    Implement ViewModel, Rv, Adapter on Female

--- app/src/main/kotlin/io/github/ovso/hometraining/view/ui/female/FemaleViewModel.kt
+++ app/src/main/kotlin/io/github/ovso/hometraining/view/ui/female/FemaleViewModel.kt
@@ -1,6 +1,15 @@
 package io.github.ovso.hometraining.view.ui.female
 
+import androidx.databinding.ObservableArrayList
+import io.github.ovso.hometraining.R
+import io.github.ovso.hometraining.utils.ResourceProvider
 import io.github.ovso.hometraining.view.base.DisposableViewModel
 
 class FemaleViewModel : DisposableViewModel() {
+  val items = ObservableArrayList<String>()
+
+  init {
+    items.addAll(ResourceProvider.getStringArray(R.array.tabs_title_female).toMutableList())
+  }
+
 }
--- app/src/main/kotlin/io/github/ovso/hometraining/view/ui/male/MaleViewModel.kt
+++ app/src/main/kotlin/io/github/ovso/hometraining/view/ui/male/MaleViewModel.kt
@@ -4,19 +4,11 @@ import androidx.databinding.ObservableArrayList
 import io.github.ovso.hometraining.R
 import io.github.ovso.hometraining.utils.ResourceProvider
 import io.github.ovso.hometraining.view.base.DisposableViewModel
-import io.reactivex.disposables.CompositeDisposable
 
 class MaleViewModel : DisposableViewModel() {
-  private val compositeDisposable by lazy { CompositeDisposable() }
-
   val items = ObservableArrayList<String>()
 
   init {
     items.addAll(ResourceProvider.getStringArray(R.array.tabs_title_male).toMutableList())
-  }
-
-  override fun onCleared() {
-    super.onCleared()
-    compositeDisposable.clear()
   }
 }
