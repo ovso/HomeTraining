fb86d1507106b82d30d14e76cd5ba9eb18ebb0b2
jaeho <ovsoce@gmail.com>
2019-12-01 19:13:38 +0900

    Play video on PlayerActivity

--- app/src/main/java/io/github/ovso/hometraining/view/ui/player/PlayerActivity.kt
+++ app/src/main/java/io/github/ovso/hometraining/view/ui/player/PlayerActivity.kt
@@ -1,29 +1,19 @@
 package io.github.ovso.hometraining.view.ui.player
 
 import android.os.Bundle
-import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
-import android.view.View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-import android.view.View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-import android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-import android.view.View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-import android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-import android.view.View.SYSTEM_UI_FLAG_LOW_PROFILE
 import androidx.appcompat.app.AppCompatActivity
 import io.github.ovso.hometraining.R
-import kotlinx.android.synthetic.main.activity_player.fullscreen_content
+import kotlinx.android.synthetic.main.activity_player.*
 
 class PlayerActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_player)
+    youtube_player.enterFullScreen()
   }
 
-  override fun onPostCreate(savedInstanceState: Bundle?) {
-    super.onPostCreate(savedInstanceState)
-    enableFullscreen()
-  }
-
+/*
   private fun enableFullscreen() {
     fullscreen_content.apply {
       systemUiVisibility =
@@ -44,4 +34,5 @@ class PlayerActivity : AppCompatActivity() {
             SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
     }
   }
+*/
 }
