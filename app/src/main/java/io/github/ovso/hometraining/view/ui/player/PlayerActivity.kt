d11ab0bcd4e8f6f41f03bfba5fff9ca9ce2bcc57
jaeho <ovsoce@gmail.com>
2019-12-07 22:22:38 +0900

    Edit PlayerActivity

--- app/src/main/java/io/github/ovso/hometraining/view/ui/player/PlayerActivity.kt
+++ app/src/main/java/io/github/ovso/hometraining/view/ui/player/PlayerActivity.kt
@@ -1,10 +1,17 @@
 package io.github.ovso.hometraining.view.ui.player
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import io.github.ovso.hometraining.R
 import io.github.ovso.hometraining.databinding.ActivityPlayerBinding
 import io.github.ovso.hometraining.view.base.DataBindingActivity2
+import kotlinx.android.synthetic.main.activity_player.btn_web_back
+import kotlinx.android.synthetic.main.activity_player.btn_web_forward
+import kotlinx.android.synthetic.main.activity_player.btn_web_share
 import kotlinx.android.synthetic.main.activity_player.wv_player
 
 @SuppressLint("SetJavaScriptEnabled")
@@ -12,12 +19,64 @@ class PlayerActivity : DataBindingActivity2<ActivityPlayerBinding>(
     layoutResId = R.layout.activity_player,
     viewModelCls = PlayerViewModel::class.java
 ) {
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    setupWebView()
+    addEvent()
+  }
+
+  private fun addEvent() {
+    btn_web_back.setOnClickListener { wv_player.goBack() }
+    btn_web_forward.setOnClickListener { wv_player.goForward() }
+    btn_web_share.setOnClickListener {
+      startActivity(
+          Intent.createChooser(Intent(Intent.ACTION_SEND).apply {
+            type = "text/plain"
+            putExtra(Intent.EXTRA_TEXT, wv_player.url)
+          }, "")
+      )
+    }
+  }
+
+  private fun setupWebView() {
     with(wv_player) {
       settings.javaScriptEnabled = true
-      webViewClient = android.webkit.WebViewClient()
-      webChromeClient = android.webkit.WebChromeClient()
+      webViewClient = ViewClient()
+      webChromeClient = ChromeClient()
+    }
+  }
+
+  class ViewClient : WebViewClient()
+
+  inner class ChromeClient :
+      WebChromeClient() {
+    override fun onProgressChanged(
+      view: WebView?,
+      newProgress: Int
+    ) {
+      super.onProgressChanged(view, newProgress)
+      if (newProgress == 100) {
+        view?.let {
+          binding?.viewModel?.canGoBackOb?.set(it.canGoBack())
+          binding?.viewModel?.canGoForwardOb?.set(it.canGoForward())
+        }
+      }
+
+    }
+
+  }
+
+  override fun onDestroy() {
+    wv_player.destroy()
+    super.onDestroy()
+  }
+
+  override fun onBackPressed() {
+    if (wv_player.canGoBack()) {
+      wv_player.goBack()
+    } else {
+      super.onBackPressed()
     }
   }
 }
--- app/src/main/java/io/github/ovso/hometraining/view/ui/player/PlayerViewModel.kt
+++ app/src/main/java/io/github/ovso/hometraining/view/ui/player/PlayerViewModel.kt
@@ -1,5 +1,6 @@
 package io.github.ovso.hometraining.view.ui.player
 
+import androidx.databinding.ObservableBoolean
 import androidx.databinding.ObservableField
 import io.github.ovso.hometraining.utils.RxBusBehavior
 import io.github.ovso.hometraining.utils.RxBusBehavior.VideoId
@@ -9,6 +10,8 @@ private const val BASE_URL_YOUTUBE = "https://www.youtube.com/watch?v="
 
 class PlayerViewModel : DisposableViewModel() {
   val videoUrlOb = ObservableField<String>()
+  val canGoBackOb = ObservableBoolean(false)
+  val canGoForwardOb = ObservableBoolean(false)
 
   init {
     addDisposable(
