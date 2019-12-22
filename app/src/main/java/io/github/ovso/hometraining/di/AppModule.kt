9d386d27b0562300129cd63c853139d7f7ed3d70
jaeho <ovsoce@gmail.com>
2019-12-22 23:57:21 +0900

    Edit di

--- app/src/main/java/io/github/ovso/hometraining/di/AppModule.kt
+++ app/src/main/java/io/github/ovso/hometraining/di/AppModule.kt
@@ -1,8 +1,10 @@
 package io.github.ovso.hometraining.di
 
+import io.github.ovso.hometraining.data.api.SearchRequest
 import io.github.ovso.hometraining.view.ui.video.VideoAdapter
 import org.koin.dsl.module
 
 val appModule = module {
   factory { VideoAdapter() }
+  single { SearchRequest() }
 }
