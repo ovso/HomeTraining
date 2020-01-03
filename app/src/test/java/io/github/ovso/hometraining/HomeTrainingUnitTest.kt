71721ca767a92abc0f1e120b73b4590c3f3d5eb1
jaeho <ovsoce@gmail.com>
2020-01-03 22:56:25 +0900

    Remove channels_test

--- app/src/test/java/io/github/ovso/hometraining/HomeTrainingUnitTest.kt
+++ app/src/test/java/io/github/ovso/hometraining/HomeTrainingUnitTest.kt
@@ -49,39 +49,4 @@ class HomeTrainingUnitTest {
             }
         )
   }
-  @Test
-  fun channels_test() {
-
-    fun getApiKey(): String {
-      val keys =
-        "AIzaSyA4pdIQO-74kZv7MLpPZs13oEYq2w5ki4E//AIzaSyCDlPMTU-TsKp8k7t6875jkAIRWrl2XCfE//AIzaSyCe8fJ3dw_8YzFq1L7X3Iip9Bs_KZ66bNM//AIzaSyBT2wy_F43ouGtgmNBmklik6qYHYFIVtbA"
-      return keys.split("//")[0]
-    }
-
-    searchRequest.api()
-        .channels(
-            hashMapOf(
-                "q" to "홈트레이닝",
-                "maxResults" to 1,
-                "order" to "viewCount",
-                "type" to "video",
-                "videoSyndicated" to "any",
-                "key" to getApiKey(),
-                "part" to "snippet",
-                "fields" to "items(id,snippet(title,thumbnails(medium)))"
-            )
-        )
-        .subscribeOn(TestSchedulerProvider.io())
-        .observeOn(TestSchedulerProvider.ui())
-        .subscribe(
-            {
-              //              println(it)
-              println("검색 성공 <<<<")
-            },
-            {
-              println((it as? HttpException)?.response()?.errorBody()?.string())
-              println("검색 실패 <<<<")
-            }
-        )
-  }
 }
