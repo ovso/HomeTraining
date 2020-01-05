4531b7d5d2d89f6a0c413beffb2a9ed843c79e66
jaeho <ovsoce@gmail.com>
2020-01-06 06:53:06 +0900

    Edit splash

--- app/src/main/java/io/github/ovso/hometraining/view/ui/splash/SplashActivity.kt
+++ app/src/main/java/io/github/ovso/hometraining/view/ui/splash/SplashActivity.kt
@@ -1,7 +1,6 @@
 package io.github.ovso.hometraining.view.ui.splash
 
 import android.os.Bundle
-import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import io.github.ovso.hometraining.exts.startActivity
 import io.github.ovso.hometraining.view.ui.main.MainActivity
@@ -10,9 +9,7 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Handler().postDelayed({
-            startActivity<MainActivity>()
-            finish()
-        }, 1000)
+        startActivity<MainActivity>()
+        finish()
     }
 }
