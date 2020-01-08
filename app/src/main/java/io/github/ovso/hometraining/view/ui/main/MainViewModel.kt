c7f6ca677448d681a47e1e670254692509e37617
jaeho <ovsoce@gmail.com>
2020-01-08 23:18:27 +0900

    Edit layout, create MainViewModel

--- app/src/main/java/io/github/ovso/hometraining/view/ui/main/MainViewModel.kt
+++ app/src/main/java/io/github/ovso/hometraining/view/ui/main/MainViewModel.kt
@@ -2,5 +2,5 @@ package io.github.ovso.hometraining.view.ui.main
 
 import io.github.ovso.hometraining.view.base.DisposableViewModel
 
-class MainViewModel: DisposableViewModel() {
+class MainViewModel : DisposableViewModel() {
 }
