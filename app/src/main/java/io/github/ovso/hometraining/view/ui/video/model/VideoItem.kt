13b3e48c9bcaa625687ae94decce7baa142258b1
jaeho <ovsoce@gmail.com>
2020-01-16 07:46:05 +0900

    Apply databinding on VideoActivity list

--- app/src/main/java/io/github/ovso/hometraining/view/ui/video/model/VideoItem.kt
+++ app/src/main/java/io/github/ovso/hometraining/view/ui/video/model/VideoItem.kt
@@ -1,12 +1,17 @@
 package io.github.ovso.hometraining.view.ui.video.model
 
+import android.view.View
+import io.github.ovso.hometraining.exts.startActivity
+import io.github.ovso.hometraining.utils.RxBusBehavior
+import io.github.ovso.hometraining.view.ui.player2.Player2Activity
+
 data class VideoItem(
     val imgUrl: String = "",
     val videoId: String = ""
 ) {
 
-}
-
-fun to(): VideoItem {
-    return VideoItem()
+    fun onClick(v: View) {
+        RxBusBehavior.send(RxBusBehavior.send(this))
+        v.context.startActivity<Player2Activity>()
+    }
 }
